d62208f559f9f742a2f86be8d6d99c5c0afd8f37
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2021-02-26 21:03:52 +0530

    Add files via upload

---
+++ Instagram clone/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC03EmKuZnCIF_aHB9i-IQuQgy6UbeB3Po",
+    authDomain: "insta-clone-e17bf.firebaseapp.com",
+    projectId: "insta-clone-e17bf",
+    storageBucket: "insta-clone-e17bf.appspot.com",
+    messagingSenderId: "612682511924",
+    appId: "1:612682511924:web:642710b5b4a880adb7fc61",
+    measurementId: "G-T24CK28ZE1"
+  };
